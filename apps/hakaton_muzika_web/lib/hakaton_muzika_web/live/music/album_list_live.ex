cfdcde7bba2e23d14418326e4f3987162323c93f
DujaLegendaa <dorizmeo2@gmail.com>
2022-11-29 18:01:04 +0100

    home page + albums

--- apps/hakaton_muzika_web/lib/hakaton_muzika_web/live/music/album_list_live.ex
+++ apps/hakaton_muzika_web/lib/hakaton_muzika_web/live/music/album_list_live.ex
@@ -5,12 +5,4 @@ defmodule HakatonMuzikaWeb.Music.AlbumListLive do
     {:ok, socket}
   end
   
-  def album_card(%{id: _, name: _, cover: _} = assigns) do
-~H"""
-    <a href={"/albums/#{@id}"}>
-      <img src={HakatonMuzikaWeb.B3.get_url(@cover)} class="rounded-xl"/>
-      <p class="font-bold"><%= cut_text(@name) %></p>
-    </a>
-    """
-  end
 end
