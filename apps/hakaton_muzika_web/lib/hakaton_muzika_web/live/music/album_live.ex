c882aa86c239845a1c869e37cc303ca133e1bc29
DujaLegendaa <dorizmeo2@gmail.com>
2022-11-30 16:04:40 +0100

    album

--- apps/hakaton_muzika_web/lib/hakaton_muzika_web/live/music/album_live.ex
+++ apps/hakaton_muzika_web/lib/hakaton_muzika_web/live/music/album_live.ex
@@ -41,6 +41,43 @@ defmodule HakatonMuzikaWeb.Music.AlbumLive do
     {:noreply, socket}
   end
 
+  def song_card(%{id: _, title: _, duration: _, track: _, cover: _, album_name: _, playlists: _} = assigns) do
+~H"""
+    <div class="relative flex px-10 py-2 gap-x-2">
+      <img class="w-[50px] lg:w-[80px]" src={HakatonMuzikaWeb.B3.get_url @cover} />
+      <div class="grid grid-cols-3 items-center w-full">
+      <div>
+        <p class="text-sm lg:text-md"> <%= cut_text @title %> </p>
+      </div>
+        <p class="justify-self-center text-sm lg:text-md font-bold"> <%= cut_text @album_name %></p>
+      </div>
+      <%= if @playlists do %>
+        <div class="hover:cursor-pointer" phx-click={JS.toggle(to: "#menu-#{Integer.to_string(@id)}", in: {"ease-in duration-150", "opacity-0", "opacity-100"}, out: "fade-out-scale")} >
+          <FontAwesome.LiveView.icon name="plus" type="solid" class="w-6 h-6 fill-white"/>
+        </div>
+      <% end %>
+    <.playlist_menu id={@id} playlists={@playlists} />
+    </div>
+    """
+  end
+
+  def playlist_menu(%{id: _, playlists: _} = assigns) do
+~H"""
+    <%= if @playlists do %>
+      <div class="hidden z-10 absolute top-9 right-10 p-2 rounded-xl bg-slate-500 w-[10%]" id={"menu-#{Integer.to_string(@id)}"}>
+        <%= for playlist <- @playlists do %>
+          <p class="hover:cursor-pointer" phx-click="add-to-playlist" phx-value-playlist_id={playlist.id} phx-value-song_id={@id}> <%= playlist.name %> </p>
+        <% end %>
+      </div>
+    <% end %>
+    """
+  end
+
+
+
+end
+
+  """
   def song_card(%{id: _, title: _, duration: _, track: _, album_name: _, album_cover: _, playlists: _} = assigns) do
 ~H"""
     <div class="relative">
@@ -56,29 +93,6 @@ defmodule HakatonMuzikaWeb.Music.AlbumLive do
             </div>
           </div>
         </div>
-        <%= if @playlists do %>
-          <div class="hover:cursor-pointer" phx-click={JS.toggle(to: "#menu-#{@id}", in: {"ease-in duration-150", "opacity-0", "opacity-100"}, out: "fade-out-scale")} >
-            <FontAwesome.LiveView.icon name="plus" type="solid" class="w-6 h-6 fill-white"/>
-          </div>
-        <% end %>
       </div>
-    <.playlist_menu id={@id} playlists={@playlists} />
     </div>
     """
-  end
-
-  def playlist_menu(%{id: _, playlists: _} = assigns) do
-~H"""
-    <%= if @playlists do %>
-      <div class="hidden z-10 absolute top-9 right-6 p-2 rounded-xl bg-slate-500" id={"menu-#{Integer.to_string(@id)}"}>
-        <%= for playlist <- @playlists do %>
-          <p class="hover:cursor-pointer" phx-click="add-to-playlist" phx-value-playlist_id={playlist.id} phx-value-song_id={@id}> <%= playlist.name %> </p>
-        <% end %>
-      </div>
-    <% end %>
-    """
-  end
-
-
-
-end
